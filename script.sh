9c4e491ec5626dc80df183375117e7326345a61f
KSDeng <kaisheng.deng@outlook.com>
2021-12-19 20:48:04 +0800

    modify scripts to use xcnd35-xcnd39

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 
 if [ $1 == "--sql" ];then
 	echo "execute sql"
-	${cockroachPath} sql --insecure --host=xcnd30 --execute="$2"
+	${cockroachPath} sql --insecure --host=xcnd35 --execute="$2"
 elif [ $1 == "--sqlfromfile" ];then
 	echo "execute sql from file"
-	${cockroachPath} sql --insecure --host=xcnd30 --file=$2
+	${cockroachPath} sql --insecure --host=xcnd35 --file=$2
 elif [ $1 == "--nodestatus" ];then
 	echo "show node status"
-	${cockroachPath} node status --insecure --host=xcnd30
+	${cockroachPath} node status --insecure --host=xcnd35
 elif [ $1 == "--startCockroachDB" ];then
 	echo "starting cockroachDB..."
 	${projectRootPath}/shell/startCockroachDB.sh $2
@@ -19,13 +19,13 @@ elif [ $1 == "--copyDataToCockroachDB" ];then
 elif [ $1 == "--refreshCockroachDBData" ];then
 	echo "importing data to cockroachDB..."
 	refreshDataScript=/temp/DatabaseTuning/sql/refreshData.sql
-	${cockroachPath} sql --insecure --host=xcnd30 --file=${refreshDataScript}
+	${cockroachPath} sql --insecure --host=xcnd35 --file=${refreshDataScript}
 elif [ $1 == "--help" ];then
 	echo "Options:"
 	echo "	--sql				Execute sql command 			(param1: sql command you want to execute)"
 	echo "	--sqlfromfile			Execute sql command from file 		(param1: sql file you want to execute)"
 	echo "	--nodestatus			Show node status"
-	echo "	--startCockroachDB		Start cockroachDB cluster		(param1: node number, 1,2,3,4,5 for xcnd30-xcnd34 respectively, must be execute in xcnd34 lastly)"
-	echo "	--copyDataToCockroachDB		Copy data files to cockroachDB file system, must start cockroachDB cluster first		(param1: node number, 1,2,3,4,5 for xcnd30-xcnd34 respectively)"
+	echo "	--startCockroachDB		Start cockroachDB cluster		(param1: node number, 1,2,3,4,5 for xcnd35-xcnd39 respectively, must be execute in xcnd34 lastly)"
+	echo "	--copyDataToCockroachDB		Copy data files to cockroachDB file system, must start cockroachDB cluster first		(param1: node number, 1,2,3,4,5 for xcnd35-xcnd34 respectively)"
 	echo "	--refreshCockroachDBData	Refresh the data in cockroachDB, must copy data files to cockroachDB file system first"
 fi
--- shell/startCockroachDB.sh
+++ shell/startCockroachDB.sh
@@ -2,54 +2,60 @@
 
 . /temp/DatabaseTuning/shell/common.sh
 
+# xcnd35: 192.168.48.254
+# xcnd36: 192.168.48.255
+# xcnd37: 192.168.51.0
+# xcnd38: 192.168.51.1
+# xcnd39: 192.168.51.2
+
 case $1 in
 	1)  echo 'start db 1'
 	echo ${cockroachPath}
 	${cockroachPath} start \
 	--insecure \
 	--store=${root}/node1 \
-	--listen-addr=192.168.48.249:26000 \
-	--http-addr=192.168.48.249:8090 \
-	--join=192.168.48.249:26000,192.168.48.250:26000,192.168.48.251:26000,192.168.48.252:26000,192.168.48.253:26000 \
+	--listen-addr=xcnd35:26257 \
+	--http-addr=xcnd35:8080 \
+	--join=xcnd35:26257,xcnd36:26257,xcnd37:26257,xcnd38:26257,xcnd39:26257 \
 	--background
 	;;
 	2)  echo 'start db 2'
 	${cockroachPath} start \
 	--insecure \
 	--store=${root}/node2 \
-	--listen-addr=192.168.48.250:26000 \
-	--http-addr=192.168.48.250:8090 \
-	--join=192.168.48.249:26000,192.168.48.250:26000,192.168.48.251:26000,192.168.48.252:26000,192.168.48.253:26000 \
+	--listen-addr=xcnd36:26257 \
+	--http-addr=xcnd36:8080 \
+	--join=xcnd35:26257,xcnd36:26257,xcnd37:26257,xcnd38:26257,xcnd39:26257 \
 	--background
 	;;
 	3)  echo 'start db 3'
 	${cockroachPath} start \
 	--insecure \
 	--store=${root}/node3 \
-	--listen-addr=192.168.48.251:26000 \
-	--http-addr=192.168.48.251:8090 \
-	--join=192.168.48.249:26000,192.168.48.250:26000,192.168.48.251:26000,192.168.48.252:26000,192.168.48.253:26000 \
+	--listen-addr=xcnd37:26257 \
+	--http-addr=xcnd37:8080 \
+	--join=xcnd35:26257,xcnd36:26257,xcnd37:26257,xcnd38:26257,xcnd39:26257 \
 	--background
 	;;
 	4)  echo 'start db 4'
 	${cockroachPath} start \
 	--insecure \
 	--store=${root}/node4 \
-	--listen-addr=192.168.48.252:26000 \
-	--http-addr=192.168.48.252:8090 \
-	--join=192.168.48.249:26000,192.168.48.250:26000,192.168.48.251:26000,192.168.48.252:26000,192.168.48.253:26000 \
+	--listen-addr=xcnd38:26257 \
+	--http-addr=xcnd38:8080 \
+	--join=xcnd35:26257,xcnd36:26257,xcnd37:26257,xcnd38:26257,xcnd39:26257 \
 	--background
 	;;
 	5)  echo 'start db 5'
 	${cockroachPath} start \
 	--insecure \
 	--store=${root}/node5 \
-	--listen-addr=192.168.48.253:26000 \
-	--http-addr=192.168.48.253:8090 \
-	--join=192.168.48.249:26000,192.168.48.250:26000,192.168.48.251:26000,192.168.48.252:26000,192.168.48.253:26000 \
+	--listen-addr=xcnd39:26257 \
+	--http-addr=xcnd39:8080 \
+	--join=xcnd35:26257,xcnd36:26257,xcnd37:26257,xcnd38:26257,xcnd39:26257 \
 	--background
 
-	${cockroachPath} init --insecure --host=192.168.48.253:26000
+	${cockroachPath} init --insecure --host=xcnd39:26257
 	;;
 	*)  echo 'You do not select a number between 1 to 5'
 	;;
