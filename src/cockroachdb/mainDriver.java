01a8da52bae9b1af09631e8242fefdf0a4913b4d
KSDeng <kaisheng.deng@outlook.com>
2021-12-20 13:05:51 +0800

    finish parallel execution demo in mainDriver.java

--- src/cockroachdb/mainDriver.java
+++ src/cockroachdb/mainDriver.java
@@ -6,10 +6,12 @@ import java.io.FileNotFoundException;
 import java.sql.*;
 import javax.sql.DataSource;
 import com.zaxxer.hikari.*;
+import java.util.concurrent.*;
 
 public class mainDriver {
 	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		try {
 
 			HikariConfig config = new HikariConfig();
@@ -26,19 +28,41 @@ public class mainDriver {
 
 			Connection conn = ds.getConnection();
 
-			// testing...
-			Statement stat = conn.createStatement();
-			stat.execute("select * from district");
-			ResultSet res = stat.getResultSet();
+			// Task with return value (ResultSet)
+			FutureTask<ResultSet> futureTask = new FutureTask<>(()-> {
+				Statement stmt = conn.createStatement();
+				stmt.execute("select * from district limit 10");
+				return stmt.getResultSet();
+			});
+
+			FutureTask<ResultSet> futureTask2 = new FutureTask<>(()-> {
+				Statement stmt = conn.createStatement();
+				stmt.execute("select * from order_ limit 10");
+				return stmt.getResultSet();
+			});
+
+			// Task without return value
+			// pass a runnable instance to Thread()
+
+			// run t1 and t2 in parallel
+			Thread t1 = new Thread(futureTask);
+			t1.start();
+
+			Thread t2 = new Thread(futureTask2);
+			t2.start();
+
+			// if t2 has to wait for t1, use t1.join()
+
+			// the printing process must be sequential
+			// get result of t1 
+			ResultSet res = futureTask.get();
 			ResultSetMetaData rsmd = res.getMetaData();
 			int columnNumber = rsmd.getColumnCount();
-			
 			for (int i = 1; i <= columnNumber; ++i) {
 				if (i > 1) System.out.print("\t");
 				System.out.print(rsmd.getColumnName(i));
 			}
 			System.out.println("");
-
 			while (res.next()) {
 				for (int i = 1; i <= columnNumber; ++i) {
 					if (i > 1) System.out.print("\t");
@@ -46,9 +70,28 @@ public class mainDriver {
 				}
 				System.out.println("");
 			}	
+			System.out.println("");
 
-		} catch (Exception e) {
-			System.err.println(e);
+			// get result of t2
+			ResultSet res2 = futureTask2.get();
+			ResultSetMetaData rsmd2 = res2.getMetaData();
+			columnNumber = rsmd2.getColumnCount();
+			for (int i = 1; i <= columnNumber; ++i) {
+				if (i > 1) System.out.print("\t");
+				System.out.print(rsmd2.getColumnName(i));
+			}
+			System.out.println("");
+			while (res2.next()) {
+				for (int i = 1; i <= columnNumber; ++i) {
+					if (i > 1) System.out.print("\t");
+					System.out.print(res2.getString(i));
+				}
+				System.out.println("");
+			}	
+			System.out.println("");
+
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
 		}
 
 	}
