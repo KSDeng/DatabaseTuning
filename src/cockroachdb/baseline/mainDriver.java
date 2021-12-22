52cfa756f1e743b2c5cd88afe36cfec04beaed41
KSDeng <kaisheng.deng@outlook.com>
2021-12-22 10:55:56 +0800

    modify mainDriver program

--- src/cockroachdb/baseline/mainDriver.java
+++ src/cockroachdb/baseline/mainDriver.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 
-public class mainDirver {
+public class mainDriver {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
@@ -17,9 +17,12 @@ public class mainDirver {
 
 		while (reader.hasNextLine()) {
 			String str = reader.nextLine();
+			if (str.length() == 0) {
+				continue;
+			}
 			String[] values = str.split(",");
 
-			switch (values[0]) {
+			switch (values[0].charAt(0)) {
 				case 'N': {
 					System.out.println("NewOrder Xact");
 					break;
