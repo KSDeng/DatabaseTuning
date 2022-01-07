40ccf4d3e2882764b5a6fb50bc630c9ae488588a
KSDeng <kaisheng.deng@outlook.com>
2022-01-07 13:25:11 +0800

    modify script.sh

--- script.sh
+++ script.sh
@@ -25,6 +25,13 @@ elif [ $1 == "--startCockroachDB" ];then
 	fi
 	echo "starting cockroachDB..."
 	${projectRootPath}/shell/startCockroachDB.sh $2
+elif [ $1 == "--startAllCockroachDB" ];then
+	for ((c = 0; c < 5; c++))
+	do
+		server_no=$(expr $c + 5)
+		para=$(expr $c + 1)
+		ssh kaisheng@xcnd3${server_no}.comp.nus.edu.sg "${projectRootPath}/shell/startCockroachDB.sh $para &"
+	done
 elif [ $1 == "--copyDataToCockroachDB" ];then
 	if [ $# != 2 ];then
 		echo "missing parameters, use --help option to see more information"
@@ -53,6 +60,13 @@ elif [ $1 == "--quitCockroachDB" ];then
 	fi
 	echo "quiting cockroachDB..."
 	${projectRootPath}/shell/quitCockroachDB.sh $2
+elif [ $1 == "--quitAllCockroachDB" ];then
+	for ((c = 0; c < 5; c++))
+	do
+		server_no=$(expr $c + 5)
+		para=$(expr $c + 1)
+		ssh kaisheng@xcnd3${server_no}.comp.nus.edu.sg "${projectRootPath}/shell/quitCockroachDB.sh $para"
+	done
 elif [ $1 == "--runExperimentsCockroachDB" ];then
 	if [ $# != 3 ];then
 		echo "missing parameters, use --help option to see more information"
@@ -81,12 +95,14 @@ elif [ $1 == "--help" ];then
 	echo "	--sqlfromfile			Execute sql command from file 		(param1: sql file you want to execute)"
 	echo "	--nodestatus			Show node status"
 	echo "	--startCockroachDB		Start cockroachDB cluster		(param1: node number, 1,2,3,4,5 for xcnd35-xcnd39 respectively, must be execute in xcnd39 lastly)"
+	echo "	--startAllCockroachDB		Start all cockroachDB nodes (xcnd35-xcnd39)"
 	echo "	--copyDataToCockroachDB		Copy data files to cockroachDB file system, must start cockroachDB cluster first		(param1: node number, 1,2,3,4,5 for xcnd35-xcnd39 respectively)"
 	echo "	--refreshCockroachDBData	Refresh the data in cockroachDB, must copy data files to cockroachDB file system first"
 	echo "	--initDesignedTables		Init newly designed schema, will also insert data into the new schema tables, must import data to original tables first"
 	echo "	--countLines			Count the number of records in the current tables in wholesaledata, will output to countLines.txt, must init designed tables first"
 	echo "	--showSchema			Show current schema of tables in wholesaledata, will output to schema.txt, must init designed tables first"
 	echo "	--quitCockroachDB		Quit cockroachDB node, will also delete the data directory		(param1: node number, 1,2,3,4,5 for xcnd35-xcnd39 respectively)"
+	echo "	--quitAllCockroachDB		Quit all cockroachDB nodes (xcnd35-xcnd39)"
 	echo "	--runExperimentsCockroachDB	Run all experiments using cockroachDB				(param1: baseline, tuning_schema, tuning_threads or tuning_schema_and_threads, means which version of code to run. param2: A or B, means workload A or workload B respectively)"
 	echo "	--fetchLogToLocal		Fetch all the logs to current local file system, should run on local system or xcnd35		(param1: baseline, tuning_schema, tuning_threads or tuning_schema_and_threads, means which version of logs to fetch)"
 fi
